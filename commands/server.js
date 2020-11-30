93ab17c7769fe6e1872808a0bc126ff39b23eda0
Mukul Pathania <mukulpathania96@gmail.com>
2020-11-30 19:48:56 +0530

    Add an embed to the server command
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

--- commands/server.js
+++ commands/server.js
@@ -2,6 +2,27 @@ module.exports = {
 	name: 'server',
 	description: 'Display info about this server.',
 	execute(message) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+		if(message.channel.type == 'dm'){
+			message.channel.send('This command does not work in DM\'s');
+			return;
+		}
+		const helpEmbed = {
+			color: 0x00FFFF,
+			title: 'Server',
+			description: 'Details of the server',
+			fields: [
+				{
+					name: 'Server name:',
+					value: message.guild.name,
+				},
+				{
+					name: 'Total members: ',
+					value: message.guild.memberCount,
+				}
+			],
+			timestamp: new Date(),
+		}
+		message.channel.send({embed: helpEmbed});
+		// message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
 	},
 };
